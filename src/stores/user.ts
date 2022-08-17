7bae25b2104a045ec4412ebb0a582b5e97493dd6
m0nclous <m0nclous@vk.com>
2022-08-18 03:25:16 +0600

    Первичная реализация входа

---
+++ src/stores/user.ts
@@ -0,0 +1,32 @@
+import { defineStore } from 'pinia';
+import router from '@/router';
+
+export const useUserStore = defineStore({
+    id: 'user',
+
+    state: () => ({
+        token: localStorage.getItem('token')
+    }),
+
+    getters: {
+        isAuth: (state) => !!state.token
+    },
+
+    actions: {
+        async login() {
+            this.token = 'mock';
+
+            localStorage.setItem('token', this.token);
+
+            await router.push('/');
+        },
+
+        async logout() {
+            this.token = null;
+
+            localStorage.removeItem('token');
+
+            await router.push('/login');
+        }
+    }
+});
--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,36 +1,40 @@
 <template>
-    <el-row>
-        <el-col :span="3" :offset="10">
-            <CampusLogo />
-        </el-col>
-    </el-row>
+    <div>
+        <el-row>
+            <el-col :span="3" :offset="10">
+                <CampusLogo />
+            </el-col>
+        </el-row>
 
-    <el-row>
-        <el-col :span="3" :offset="10">
-            <h2>Вход</h2>
-        </el-col>
-    </el-row>
+        <el-row>
+            <el-col :span="3" :offset="10">
+                <h2>Вход</h2>
+            </el-col>
+        </el-row>
 
-    <el-row>
-        <el-col :span="3" :offset="10">
-            <el-form>
-                <el-form-item prop="login">
-                    <el-input v-model="form.username" placeholder="Логин" size="large" />
-                </el-form-item>
+        <el-row>
+            <el-col :span="3" :offset="10">
+                <el-form>
+                    <el-form-item prop="login">
+                        <el-input v-model="form.username" placeholder="Логин" size="large" />
+                    </el-form-item>
 
-                <el-form-item prop="password">
-                    <el-input v-model="form.password" placeholder="Пароль" size="large" />
-                </el-form-item>
+                    <el-form-item prop="password">
+                        <el-input v-model="form.password" placeholder="Пароль" size="large" />
+                    </el-form-item>
 
-                <el-form-item>
-                    <el-button type="success">Войти</el-button>
-                </el-form-item>
-            </el-form>
-        </el-col>
-    </el-row>
+                    <el-form-item>
+                        <el-button type="success" @click="useUserStore().login()">Войти</el-button>
+                    </el-form-item>
+                </el-form>
+            </el-col>
+        </el-row>
+    </div>
 </template>
 
 <script setup lang="ts">
+import { useUserStore } from '@/stores/user';
+
 import CampusLogo from '@/components/CampusLogo.vue';
 
 import { ref } from 'vue';
@@ -40,8 +44,3 @@ const form = ref({
     password: ''
 });
 </script>
-
-<style scoped>
-.el-form {
-}
-</style>
