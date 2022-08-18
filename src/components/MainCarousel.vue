3c75ec5a803657810035ae698753f2fe67c88d22
m0nclous <m0nclous@vk.com>
2022-08-19 01:17:38 +0600

    Добавлена каруселька на главную

---
+++ src/components/MainCarousel.vue
@@ -0,0 +1,45 @@
+<template>
+    <el-carousel class="main-carousel" :interval="5_000" type="card" height="200px">
+        <el-carousel-item v-for="item in 6" :key="item">
+            <el-card shadow="never">
+                <template #header>
+                    <el-image :src="slideImageSrc"></el-image>
+                </template>
+
+                <div>Тренажер Агронавигатора Кампус</div>
+                <div>15.03.2022</div>
+            </el-card>
+        </el-carousel-item>
+    </el-carousel>
+</template>
+
+<script lang="ts" setup>
+import slideImageSrc from '@/assets/img/slide-image.png';
+</script>
+
+<style lang="scss" scoped>
+:deep(.el-card) {
+    display: flex;
+    border: none;
+    border-radius: 0;
+
+    .el-card__body {
+        display: flex;
+        flex-direction: column;
+        justify-content: space-between;
+    }
+}
+
+.el-carousel {
+    padding-top: 1px;
+
+    &__item--card.is-active {
+        box-shadow: var(--el-box-shadow);
+    }
+}
+
+.el-image {
+    height: 202px;
+    margin: -20px;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,15 +1,17 @@
 <template>
     <main>
         <MainBanner />
+        <MainCarousel />
     </main>
 </template>
 
 <script setup lang="ts">
 import MainBanner from '@/components/MainBanner.vue';
+import MainCarousel from '@/components/MainCarousel.vue';
 </script>
 
 <style lang="scss" scoped>
-.el-image {
-    width: 100%;
+.main-carousel {
+    margin-top: 70px;
 }
 </style>
