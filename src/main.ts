eaaa3358e559fd2fa7b458628ab4227b68728fdf
m0nclous <m0nclous@vk.com>
2022-08-23 02:54:31 +0600

    Оптимизация сборки

--- src/main.ts
+++ src/main.ts
@@ -4,10 +4,6 @@ const app = createApp(App);
 import { createPinia } from 'pinia';
 app.use(createPinia());
 
-import ElementPlus from 'element-plus';
-import 'element-plus/dist/index.css';
-app.use(ElementPlus);
-
 import App from './App.vue';
 import router from './router';
 app.use(router);
--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,23 @@
 import { fileURLToPath, URL } from 'node:url';
 
-import { defineConfig } from 'vite';
+import { defineConfig, splitVendorChunkPlugin } from 'vite';
 import vue from '@vitejs/plugin-vue';
+import AutoImport from 'unplugin-auto-import/vite';
+import Components from 'unplugin-vue-components/vite';
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [vue()],
+    plugins: [
+        vue(),
+        splitVendorChunkPlugin(),
+        AutoImport({
+            resolvers: [ElementPlusResolver()]
+        }),
+        Components({
+            resolvers: [ElementPlusResolver()]
+        })
+    ],
     server: {
         watch: {
             usePolling: true
