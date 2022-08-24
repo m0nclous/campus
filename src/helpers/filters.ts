88bbde98759c605bdea4165628a75eb15a217fd4
m0nclous <m0nclous@vk.com>
2022-08-25 02:02:55 +0600

    filter update

--- src/helpers/filters.ts
+++ src/helpers/filters.ts
@@ -1,6 +1,6 @@
-export const currencyRUB = (value: number | string): string => {
+export const currency = (value: number | string, currency = 'RUB'): string => {
     value = Number(value);
     if (isNaN(value)) throw new TypeError('value is NaN');
 
-    return `${(+value).toLocaleString()} ₽`;
+    return value.toLocaleString('ru-RU', { style: 'currency', currency, maximumFractionDigits: 0 });
 };
